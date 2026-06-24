9122648e62eeef54fb50c10eecd350a6b067b156
Dev-Sarkar <plabonsarkar525@gmail.com>
2026-06-24 12:51:40 +0600

    Create auth module and test endpoint

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  @Get('test')
+  test() {
+    return {
+      message: 'Auth module is working!',
+    };
+  }
+}
